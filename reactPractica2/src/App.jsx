4da97fec3f1f061771f0fa4df6bb683fbecabd91
BrayanGB09 <bgutierrez@fwdcostarica.com>
2024-09-02 11:48:00 -0600

    cambios

---
+++ reactPractica2/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <div>
+          <button>Inicio</button>
+            <br />
+          <button>Acerca de Nosotros</button>
+            <br />
+          <button>Contacto</button>
+            <br />
+          <button>Colaboradores</button>
+            <br />
+          <button>Preguntas Frecuentes</button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
